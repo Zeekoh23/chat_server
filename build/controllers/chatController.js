cebe077d8d7cd2cf7daaa381e4eacd31dcd7dbea
Zeekoh23 <achueze61@gmail.com>
2022-05-29 22:15:04 +0100

    hey yooo

--- build/controllers/chatController.js
+++ build/controllers/chatController.js
@@ -61,25 +61,29 @@ exports.GetChat1 = (0, CatchAsync_1.default)((req, res, next) => __awaiter(void 
     });
 }));
 exports.DeleteChat = (0, HandlerFactory_1.deleteOne)(chatModel_1.Chat);
-function updatechats(req, res, next, prop) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const chat = yield chatModel_1.Chat.findOneAndUpdate({ prop: req.params.prop }, req.body, { new: true, runValidators: true });
-        if (!chat) {
-            return next(new ErrorHandling_1.ErrorHandling("No document found with that time", 404));
-        }
-        res.status(200).json({
-            status: "success",
-            data: {
-                chat,
-            },
-        });
+exports.UpdateChat1 = (0, CatchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const chat = yield chatModel_1.Chat.findOneAndUpdate({ time: req.params.time }, req.body, { new: true, runValidators: true });
+    if (!chat) {
+        return next(new ErrorHandling_1.ErrorHandling("No document found with that time", 404));
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            chat,
+        },
     });
-}
-exports.UpdateChat1 = (0, CatchAsync_1.default)((req, res, next, time) => __awaiter(void 0, void 0, void 0, function* () {
-    updatechats(req, res, next, time);
 }));
-exports.UpdateChat2 = (0, CatchAsync_1.default)((req, res, next, lastMessage) => __awaiter(void 0, void 0, void 0, function* () {
-    updatechats(req, res, next, lastMessage);
+exports.UpdateChat2 = (0, CatchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const chat = yield chatModel_1.Chat.findOneAndUpdate({ lastMessage: req.params.lastMessage }, req.body, { new: true, runValidators: true });
+    if (!chat) {
+        return next(new ErrorHandling_1.ErrorHandling("No document found with that message", 404));
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            chat,
+        },
+    });
 }));
 exports.UpdateChat3 = (0, CatchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const chat = yield chatModel_1.Chat.findOneAndUpdate({ number: req.params.number }, req.body, { new: true, runValidators: true });
@@ -93,27 +97,40 @@ exports.UpdateChat3 = (0, CatchAsync_1.default)((req, res, next) => __awaiter(vo
         },
     });
 }));
-function updateChatProperty(req, res, next, prop) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const chat = yield chatModel_1.Chat.updateMany({ email: req.params.email }, { prop: req.body.prop });
-        if (!chat) {
-            return next(new ErrorHandling_1.ErrorHandling("Could not update", 404));
-        }
-        res.status(200).json({
-            status: "success",
-            data: {
-                chat,
-            },
-        });
+exports.UpdateChatImage = (0, CatchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const chat = yield chatModel_1.Chat.updateMany({ email: req.params.email }, { image: req.body.image });
+    if (!chat) {
+        return next(new ErrorHandling_1.ErrorHandling("Could not update", 404));
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            chat,
+        },
     });
-}
-exports.UpdateChatImage = (0, CatchAsync_1.default)((req, res, next, image) => __awaiter(void 0, void 0, void 0, function* () {
-    updateChatProperty(req, res, next, image);
 }));
-exports.UpdateChatName = (0, CatchAsync_1.default)((req, res, next, name) => __awaiter(void 0, void 0, void 0, function* () {
-    updateChatProperty(req, res, next, name);
+exports.UpdateChatName = (0, CatchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const chat = yield chatModel_1.Chat.updateMany({ email: req.params.email }, { name: req.body.name });
+    if (!chat) {
+        return next(new ErrorHandling_1.ErrorHandling("Could not update", 404));
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            chat,
+        },
+    });
 }));
-exports.UpdateChatAbout = (0, CatchAsync_1.default)((req, res, next, about) => __awaiter(void 0, void 0, void 0, function* () {
-    updateChatProperty(req, res, next, about);
+exports.UpdateChatAbout = (0, CatchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const chat = yield chatModel_1.Chat.updateMany({ email: req.params.email }, { about: req.body.about });
+    if (!chat) {
+        return next(new ErrorHandling_1.ErrorHandling("Could not update", 404));
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            chat,
+        },
+    });
 }));
 exports.UpdateChat = (0, HandlerFactory_1.updateOne)(chatModel_1.Chat);
